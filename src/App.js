f47549e71e3257fcd55dee4ecbc7e46dd59f6e2a
Wagner <iamwagnerr18@gmail.com>
2022-12-10 15:17:12 -0300

    Iniando contrução do menu lateral

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Menu from './components/menu';
 
 function App() {
   return (
     <div className="App">
-
+      <Menu />
     </div>
   );
 }
---
+++ src/components/menu.js
@@ -0,0 +1,19 @@
+import './menu.css'
+import Logo98px from '../assets/logo/tm_98x98.svg'
+
+function Menu() {
+    return (
+        <div className="Menu">
+            <header>
+                <img src={Logo98px} width='40px' />
+                <nav>
+                    <ul>
+                        D
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default Menu;
