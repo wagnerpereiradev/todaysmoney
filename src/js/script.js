c4895258c6c1a4a7b1ea0baac2ef6fd32788df47
Wagner <iamwagnerr18@gmail.com>
2022-12-10 17:18:25 -0300

    Mudanças no menu lateral

--- src/js/script.js
+++ src/js/script.js
@@ -12,9 +12,21 @@ menu.addEventListener('mouseout', () => {
 function menuExpand() {
     menu.classList.remove('menu');
     menu.classList.add('menu_expanded');
+
+    for (var i = 0; i < 6; i++) {
+        itemLabel = document.querySelector('.hide');
+        itemLabel.classList.remove('hide')
+        itemLabel.classList.add('show')
+    }
 }
 
 function menuColapse() {
     menu.classList.remove('menu_expanded');
     menu.classList.add('menu');
+
+    for (var i = 0; i < 6; i++) {
+        itemLabel = document.querySelector('.show');
+        itemLabel.classList.remove('show')
+        itemLabel.classList.add('hide')
+    }
 }
